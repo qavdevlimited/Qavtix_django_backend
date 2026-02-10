6c99836fbc0d9f3c80a2eada47178bc5a1b97467
marvidik <ebubeidika@gmail.com>
2026-02-10 10:07:31 +0100

    made the secret key privatized

--- Qavtix/settings.py
+++ Qavtix/settings.py
@@ -21,12 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qt46#&n*5%r0_zt#q5aa+-v87*l16pqub0g^r=6bgolzmw7)63'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 env = environ.Env(DEBUG=(bool, False))
 environ.Env.read_env()
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env("SECRET_KEY")
+
 
 DEBUG = env("DEBUG")
 DATABASES = {

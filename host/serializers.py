5aadbbe59f3a74eaea416a52ae6abc4d1f24c6f7
marvidik <ebubeidika@gmail.com>
2026-03-31 10:07:06 +0100

    added host id to the evennt details page

--- host/serializers.py
+++ host/serializers.py
@@ -214,6 +214,7 @@ class EventDetailsSerializer(serializers.ModelSerializer):
     is_following = serializers.SerializerMethodField()
     is_trending=serializers.SerializerMethodField()
     is_filling_fast=serializers.SerializerMethodField()
+    organizer_id=serializers.IntegerField(source='host.id')
 
     class Meta:
         model = Event
@@ -221,7 +222,7 @@ class EventDetailsSerializer(serializers.ModelSerializer):
             'id', 'title', 'category', 'tags', 'event_type', 'start_datetime', 'end_datetime',
             'location_type', 'short_description', 'full_description',
             'organizer_display_name', 'organizer_description', 'public_email', 'phone_number',
-            'event_location', 'social_links', 'tickets','event_status','event_image','attendees_count','age_restriction','is_favorite','is_following','is_trending','is_filling_fast',
+            'event_location', 'social_links', 'tickets','event_status','event_image','attendees_count','age_restriction','is_favorite','is_following','is_trending','is_filling_fast','organizer_id'
         ]
     
     def get_attendees_count(self, obj):
